da3e2a33fa1007123b81b46b624aec585104e852
Meyer Tom <tom31.meyer@gmail.com>
2025-03-11 19:30:29 +0100

    new enums

---
+++ back_coaching/src/Enum/Seance.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Enum;
+
+enum Seance: string
+{
+    case SOLO = 'solo';
+    case DUO = 'duo';
+    case TRIO = 'trio';
+}
---
+++ back_coaching/src/Enum/ThemeSeance.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Enum;
+
+enum ThemeSeance: string
+{
+    case FITNESS = 'fitness';
+    case CARDIO = 'cardio';
+    case MUSCULATION = 'musculation';
+    case CROSSFIT = 'crossfit';
+}
