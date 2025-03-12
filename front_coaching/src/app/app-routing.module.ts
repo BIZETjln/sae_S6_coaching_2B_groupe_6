9075d1724a6dbcfefa99c04a36a6119c33b3072f
MaelCarrie <mael.carrie@gmail.com>
2025-03-12 12:16:20 +0100

    Commit : Ajout de la page coachs

--- front_coaching/src/app/app-routing.module.ts
+++ front_coaching/src/app/app-routing.module.ts
@@ -3,12 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ConnexionComponent } from './connexion/connexion.component';
 import { InscriptionComponent } from './inscription/inscription.component';
-
+import { CoachsComponent } from './coachs/coachs.component';
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'connexion', component: ConnexionComponent },
   { path: 'mot-de-passe-oublie', redirectTo: 'connexion' }, // Redirection temporaire
   { path: 'inscription', component: InscriptionComponent },
+  { path: 'coachs', component: CoachsComponent },
 ];
 
 @NgModule({
